f52433d4630ccdd11c65c5bccd9a3e32ba425c01
TigerClips1 <spongebob1966@proton.me>
2024-02-08 23:34:33 -0500

    added death_block

--- SRC/player.gd
+++ SRC/player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $Coyote_jump_Timer
 
 func _physics_process(delta):
 	Apply_Gravaty(delta)
@@ -16,17 +17,21 @@ func _physics_process(delta):
 	Apply_Actlation(input_axis, delta)
 	Apply_friction(input_axis, delta)
 	update_Anmation(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func Apply_Gravaty(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 func Handlejump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_pressed("ui_accept") and  velocity.y < 0:
 			velocity.y = JUMP_VELOCITY / 1
 
--- SRC/world.gd
+++ SRC/world.gd
@@ -3,3 +3,9 @@ extends Node2D
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
+
+@onready var player = $Player
+
+func _on_death_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://Sceans/lost_screen.tscn")
