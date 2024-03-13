4aa3aa3c51101d841bdddc912cfd61b6f43c1301
TigerClips1 <spongebob1966@proton.me>
2024-03-12 21:43:08 -0400

    UpDaTe

---
+++ SRC/warning.gd
@@ -0,0 +1,12 @@
+extends Container
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	RenderingServer.set_default_clear_color(Color.BLACK)
+	await get_tree().create_timer(5).timeout
+	get_tree().paused = true
+	await  LevelFade._fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Sceans/Level1.tscn")
+	LevelFade._fade_from_black()
